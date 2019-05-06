4820d1d31c93dae1170af08b1b7f98858cc5a762
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-05-05 20:58:49 -0700

    Help sidebar
    Added a help sidebar

--- RiemannWeb/wwwroot/js/app.js
+++ RiemannWeb/wwwroot/js/app.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     video = document.getElementById("video");
     snapButton = document.getElementById("snapButton");
     errorMsgElement = document.querySelector("span#errorMsg");
+    helpMenu = document.querySelector("#help");
 
     init();
 });
@@ -22,6 +23,7 @@ async function init() {
         window.stream = stream;
         video.srcObject = stream;
     } catch (e) {
+        errorMsgElement.style.display = "block";
         errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
     }
 }
@@ -36,4 +38,13 @@ function retake() {
     video.play();
     snapButton.onclick = snap;
     snapButton.value = "Capture";
+}
+
+function helpToggle() {
+    if (helpMenu.style.display === "none") {
+        helpMenu.style.display = "block";
+    }
+    else {
+        helpMenu.style.display = "none";
+    }
 }
